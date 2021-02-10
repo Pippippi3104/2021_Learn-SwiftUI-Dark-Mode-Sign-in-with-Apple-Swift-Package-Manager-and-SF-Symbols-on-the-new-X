9faeff4cf15eb8d3dae63cf6f6b7fbd432f1da2d
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2021-02-10 19:08:44 +0900

    # note

---
+++ src/Section03/SwiftFun.playground/Contents.swift
@@ -0,0 +1,31 @@
+
+// Math and Doubles
+
+var height: Int = 47
+let rideLimit: Int = 48
+
+if height < rideLimit {
+    let needToGrow: Int = rideLimit - height
+    print(needToGrow)
+}
+
+
+var f: Double = 100.0
+var g: Double = ((f - 32.0) * (5.0/9.0))
+
+
+var daysInAYear: Double = 365.0
+var daysInAWeek: Double = 7.0
+var weeksInAYear: Double = (daysInAYear / daysInAWeek)
+var weeksInAYearInt: Int = (Int(daysInAYear) / Int(daysInAWeek))
+
+
+// Do math that has FLoat, Int, and Double in one Line
+
+var a: Int = 6
+var b: Double = 7.77777777777777
+var c = Float(6)
+
+Double(a) * b * Double(c)
+
+
