a17286812309e87df8a4f9dfbbaff4cbfffdd25e
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2021-02-12 18:27:34 +0900

    # note

--- src/Section09/Bitcoin Price Tracker/Bitcoin Price Tracker/BitcoinViewController.swift
+++ src/Section09/Bitcoin Price Tracker/Bitcoin Price Tracker/BitcoinViewController.swift
@@ -12,8 +12,19 @@ class BitcoinViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // MARK: use API
+        // Crypto Compare(https://min-api.cryptocompare.com/documentation)
+        if let url = URL(string: "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR") {
+            URLSession.shared.dataTask(with: url) {
+                (data: Data?, response: URLResponse?, error: Error?) in
+                if error == nil {
+                    print("It's working!")
+                } else {
+                    print("We have error.")
+                }
+            }.resume()
+        }
     }
-
-
 }
 
