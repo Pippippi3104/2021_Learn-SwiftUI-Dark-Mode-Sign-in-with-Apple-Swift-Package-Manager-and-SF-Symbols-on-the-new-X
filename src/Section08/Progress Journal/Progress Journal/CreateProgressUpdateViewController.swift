0ddd47477d0ef63c29c9183f8fa87949b02eadd2
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2021-02-12 15:11:41 +0900

    # note

--- src/Section08/Progress Journal/Progress Journal/CreateProgressUpdateViewController.swift
+++ src/Section08/Progress Journal/Progress Journal/CreateProgressUpdateViewController.swift
@@ -7,12 +7,15 @@
 
 import UIKit
 
-class CreateProgressUpdateViewController: UIViewController {
+// add (UIImagePickerControllerDelegate, UINavigationControllerDelegate) using pickerController.delegate = self
+class CreateProgressUpdateViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     // MARK: Init
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var addButton: UIButton!
+    
+    var pickerController = UIImagePickerController()
     
     // MARK: loading view
     override func viewDidLoad() {
@@ -20,12 +23,25 @@ class CreateProgressUpdateViewController: UIViewController {
 
         // fixed button UI image
         addButton.layer.cornerRadius = addButton.frame.size.height / 2
+        
+        pickerController.delegate = self
     }
     
     // MARK: actions
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        <#code#>
+    }
+    
+    // MARK: button actions
     @IBAction func existingPhotoTapped(_ sender: Any) {
+        // run (need Info.plist change before running)
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true, completion: nil)
     }
     @IBAction func cameraTapped(_ sender: Any) {
+        // run (need Info.plist change before running)
+        pickerController.sourceType = .camera
+        present(pickerController, animated: true, completion: nil)
     }
     @IBAction func addTapped(_ sender: Any) {
     }
