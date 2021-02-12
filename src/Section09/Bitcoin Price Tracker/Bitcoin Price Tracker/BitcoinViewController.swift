206573bed4677637eafb456614bd28c1fd14305c
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2021-02-13 02:45:56 +0900

    # note

--- src/Section09/Bitcoin Price Tracker/Bitcoin Price Tracker/BitcoinViewController.swift
+++ src/Section09/Bitcoin Price Tracker/Bitcoin Price Tracker/BitcoinViewController.swift
@@ -9,12 +9,22 @@ import UIKit
 
 class BitcoinViewController: UIViewController {
     
+    // MARK: init
+    @IBOutlet weak var usdLabel: UILabel!
+    @IBOutlet weak var jpyLabel: UILabel!
+    @IBOutlet weak var eurLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         // MARK: use API
         // Crypto Compare(https://min-api.cryptocompare.com/documentation)
+        getPrice()
+    }
+    
+    // MARK: get Json data
+    func getPrice() {
         if let url = URL(string: "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR") {
             URLSession.shared.dataTask(with: url) {
                 (data: Data?, response: URLResponse?, error: Error?) in
@@ -22,14 +32,18 @@ class BitcoinViewController: UIViewController {
                     // convert into Json
                     if data != nil {
                         if let json = try? JSONSerialization.jsonObject(with: data!, options: []) as? [String: Double] {
-                            if let usdPrice = json["USD"] {
-                                print(usdPrice)
-                            }
-                            if let eurPrice = json["EUR"] {
-                                print(eurPrice)
-                            }
-                            if let jpyPrice = json["JPY"] {
-                                print(jpyPrice)
+                            
+                            // async actions
+                            DispatchQueue.main.async {
+                                if let usdPrice = json["USD"] {
+                                    self.usdLabel.text = "$\(usdPrice)"
+                                }
+                                if let eurPrice = json["EUR"] {
+                                    self.eurLabel.text = "$\(eurPrice)"
+                                }
+                                if let jpyPrice = json["JPY"] {
+                                    self.jpyLabel.text = "$\(jpyPrice)"
+                                }
                             }
                         }
                     }
@@ -38,6 +52,11 @@ class BitcoinViewController: UIViewController {
                 }
             }.resume()
         }
+    }
+    
+    // MARK: button actions
+    @IBAction func refreshTapped(_ sender: Any) {
+        getPrice()
     }
 }
 
